eb04d2c559d375101b609a2767de7cac2a94f3ed
Adeunle Ademefun <adekunle.ashton@gmail.com>
2023-07-12 19:32:33 -0400

    added new constant

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { ReduxApp, PersistenceApp } from './routes';
 import ReactDOM from 'react-dom/client';
+import { isUsingPersistence } from './constants';
 
-const isUsingPersistence = false;
 let app = isUsingPersistence ? <PersistenceApp /> : <ReduxApp />; //Sets Conditional to enable or disable redux-persistence since persitence is slower to load pages
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(app);
--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,7 @@
-
+export const isUsingPersistence = false;
 export const MERGED_CRUISES_API_URL = 'https://www.gmrt.org/services/GmrtCruises.php';
 export const REJECTED_CRUISES_API_URL = 'https://www.gmrt.org/services/GmrtCruises.php?is_rejected=true';
 export const UNDER_REVIEW_CRUISES_API_URL = 'https://www.gmrt.org/services/GmrtCruises.php?under_review=true';
-
-
 export const GRID_SERVER_URL = 'https://www.gmrt.org/services/GridServer?'
 export const METADATA_GRID_SERVER_URL = 'https://www.gmrt.org/services/GridServer/metadata?'
 export const URLS_GRID_SERVER_URL = 'https://www.gmrt.org/services/GridServer/urls?'
